11c9b18d6c8e0a17f8024a164e55919604704189
A-Stroem <asjnyborg@hotmail.com>
2021-11-18 11:23:25 +0100

    staff

--- code/Main.java
+++ code/Main.java
@@ -1,7 +1,13 @@
+import java.io.FileNotFoundException;
+
 public class Main{
-    public static void main(String[] args){
-        Room room=new Room();
-        room.createRoom();
-        System.out.println(room);
+    public static void main(String[] args) {
+//        Room room=new Room();
+//        room.createRoom();
+//        System.out.println(room);
+
+        Staff staff = new Staff();
+        staff.registerStaff();
+        System.out.println(staff);
     }
 }
--- code/Staff.java
+++ code/Staff.java
@@ -1,10 +1,63 @@
-public class Staff {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Staff implements Serializable {
     private String title;
     private String firstName;
     private String lastName;
     private int telephone;
     private double salary;
+    ArrayList<Staff> staffs = new ArrayList<>();
 
+
+    public void registerStaff() {
+        Staff staff = new Staff();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Title: ");
+        title = scan.next();
+        System.out.println("First Name: ");
+        firstName = scan.next();
+        System.out.println("Last Name: ");
+        lastName = scan.next();
+        System.out.println("Phone Number: ");
+        telephone = scan.nextInt();
+        System.out.println("Salary: ");
+        salary = scan.nextDouble();
+        try {
+            FileInputStream f = new FileInputStream("staff.bin");
+            ObjectInputStream ois = new ObjectInputStream(f);
+            staffs = (ArrayList<Staff>)ois.readObject();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        staffs.add(staff);
+
+        try {
+            FileOutputStream f = new FileOutputStream("staff.bin");
+            ObjectOutputStream out = new ObjectOutputStream(f);
+            out.writeObject(staffs);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Staff\n" +
+                "Title: " + title + "\n" +
+                "First Name: " + firstName + '\n' +
+                "Last Name: " + lastName + "\n" +
+                "Telephone: " + telephone + "\n" +
+                "Salary: " + salary + "\n";
+    }
 
     public String getTitle() {
         return title;
