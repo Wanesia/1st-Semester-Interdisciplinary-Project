a0c5714f7c6378af6b30ed9a7b0d03448e1e7a36
A-Stroem <asjnyborg@hotmail.com>
2021-11-18 12:59:08 +0100

    staff Options and salary

--- code/Staff.java
+++ code/Staff.java
@@ -14,16 +14,53 @@ public class Staff implements Serializable {
     public void registerStaff() {
         Staff staff = new Staff();
         Scanner scan = new Scanner(System.in);
-        System.out.println("Title: ");
-        title = scan.next();
+        System.out.println("Pick Your Title: ");
+        System.out.println("[1][General Manager]");
+        System.out.println("[2][Wait Staff]");
+        System.out.println("[3][Concierge]");
+        System.out.println("[4][Cleaning Staff]");
+        String s = scan.next();
+        if (s.equals("1")) {
+            title = "General Manager";
+        }
+            else if (s.equals("2")) {
+                title = "Wait Staff";
+                }
+                else if (s.equals("3")) {
+                    title = "Concierge";
+                    }
+                    else if (s.equals("4")){
+                        title = "Cleaning Staff";
+                        }
+                        else {
+                            System.out.println("Please pick one of the available job titles");
+                            }
+        //title = scan.next();
         System.out.println("First Name: ");
         firstName = scan.next();
         System.out.println("Last Name: ");
         lastName = scan.next();
         System.out.println("Phone Number: ");
         telephone = scan.nextInt();
-        System.out.println("Salary: ");
-        salary = scan.nextDouble();
+//        System.out.println("Salary(DKK/Year): " + salary);
+
+        if (title.equals("General Manager")) {
+            salary = 360000;
+        }
+            else if (title.equals("Wait Staff")) {
+                salary = 337000;
+            }
+                else if (title.equals("Concierge")) {
+                    salary = 326800;
+                }
+                    else if (title.equals("Cleaning Staff")){
+                        salary = 291000;
+                    }
+                        else{
+                            System.out.println("No salary to display");
+                        }
+        //salary = scan.nextDouble();
+
         try {
             FileInputStream f = new FileInputStream("staff.bin");
             ObjectInputStream ois = new ObjectInputStream(f);
